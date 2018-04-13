396ad485b8f2b8d708cbf3b5f110ab0b7dca831e
Adam Cristina <cristina_adam97@yahoo.com>
2018-04-13 08:19:45 +0300

    third commit

---
+++ Assignment2/src/main/java/ro/tuc/pt/Assignment2/App.java
@@ -0,0 +1,18 @@
+package ro.tuc.pt.Assignment2;
+
+
+import ro.tuc.pt.Assignment2.controllers.Shop;
+import ro.tuc.pt.Assignment2.models.Customer;
+import ro.tuc.pt.Assignment2.models.Server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+	
+		Shop controller = new Shop();
+		
+	}
+}
